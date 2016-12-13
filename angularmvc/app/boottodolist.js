d08e6f151194d8f2737a08de8abdf97520bd6126
Shawn Kendrot <skendrot@hotmail.com>
2016-12-12 20:46:59 -0700

    remove compiled js files

--- angularmvc/app/boottodolist.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//import {enableProdMode} from '@angular/core';
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var todolist_module_1 = require('./todolist.module');
-//enableProdMode();
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(todolist_module_1.TodoListModule);
-//# sourceMappingURL=boottodolist.js.map
